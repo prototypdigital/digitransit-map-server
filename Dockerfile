6e537da59ffdfd38184b9db7acc08a1cf5142c84
Vlatko Vlahek <vlatko@prototyp.digital>
2019-12-03 15:27:17 +0100

    minified tile server config

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,8 @@
 FROM node:6-stretch
 LABEL Prototyp version: 0.1
 
-ENV FONTSTACK_PASSWORD ""
 ENV CROATIA_OTP_URL https://api.cityrouting.e-gpp.hr/routing/v1/routers/croatia/index/graphql
 ENV WORK=/opt/hsl-map-server
-ENV NODE_OPTS ""
-
-RUN apt-get update \
-  && DEBIAN_FRONTEND=noninteractive apt-get install -y git unzip pngquant libgl1-mesa-glx libgl1-mesa-dri xserver-xorg-video-dummy libgles2-mesa libstdc++6 xorg openbox
 
 RUN mkdir -p ${WORK}
 WORKDIR ${WORK}
@@ -18,8 +13,6 @@ RUN yarn install
 
 COPY . ${WORK}
 
-# Download mbtiles manually to avoid burning bandwidth
-# RUN curl https://digitransit.sfo2.digitaloceanspaces.com/croatia.mbtiles > croatia.mbtiles
 EXPOSE 8080
 
 RUN chmod -R 777 ${WORK}
--- config.js
+++ config.js
@@ -1,40 +1,4 @@
-const sourcesUrl = "hsl-map-server:8080/map/v1/";
-const glyphsUrl = `file://${process.env.WORK}/node_modules/hsl-map-style/`;
-
 module.exports = {
-  "/map/v1/hsl-vector-map": {
-    source: "mbtiles://./croatia.mbtiles",
-    headers: {
-      "Cache-Control": "public,max-age=3600"
-    }
-  },
-  "/map/v1/hsl-map": {
-    source: {
-      protocol: "gl:",
-      query: {},
-      style: require("hsl-map-style").generateStyle({
-        sourcesUrl,
-        glyphsUrl
-      })
-    },
-    headers: {
-      "Cache-Control": "public,max-age=604800"
-    }
-  },
-  "/map/v1/hsl-map-256": {
-    source: {
-      protocol: "gl:",
-      query: { layerTileSize: 256 },
-      style: require("hsl-map-style").generateStyle({
-        sourcesUrl,
-        glyphsUrl,
-        components: { icons: { enabled: true } }
-      })
-    },
-    headers: {
-      "Cache-Control": "public,max-age=604800"
-    }
-  },
   "/map/v1/croatia-stop-map": {
     source: `otpstops://${process.env.CROATIA_OTP_URL}`,
     headers: {
