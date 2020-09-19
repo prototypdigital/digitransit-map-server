cf2cf4e6760e61cab6ae52ac2480f2b72b57c4ad
Slobodan Džakić <slobodan.dzakic@gmail.com>
2020-09-19 16:07:36 +0200

    removed some not needed files, updated config and run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-#workaround for azure DNS issue
-
-if [ -n "$MESOS_CONTAINER_NAME"  ]; then 
-  echo -e "\nsearch marathon.l4lb.thisdcos.directory" >> /etc/resolv.conf
-fi
-
 #start map server
 
 cd ${WORK} && \
-node_modules/.bin/forever start -c "node ${NODE_OPTS}" \
-node_modules/tessera/bin/tessera.js --port 8080 --config config.js --multiprocess \
--r ${WORK}/node_modules/tilelive-otp-stops/ \
+node_modules/.bin/forever start -c "node " \
+node_modules/.bin/tessera \
+--port 8080 otpstops://${CROATIA_OTP_URL} \
+--multiprocess \
+-r tilelive-otp-stops \
 && sleep 10 && node_modules/.bin/forever --fifo logs 0
